ad14ba87fa6ec103fd0fe5dd858465beced08fb0
Tomstsh <tshort2298@gmail.com>
2025-11-28 15:59:17 +0000

    Add a little placeholder for the homepage

--- src/frontend/src/pages/HomePage.tsx
+++ src/frontend/src/pages/HomePage.tsx
@@ -1,13 +1,20 @@
 import { useAuth } from '../auth/AuthContext';
+import { Card, CardContent} from '../components/ui/card';
+import { Button } from '../components/ui/button';
 
 function HomePage() {
     const { user, logout } = useAuth();
     return (
         <div>
             You are signed in {user?.username}
-            <p>
-                <button onClick={() => logout()}>Logout</button>
-            </p>
+            <div className="flex flex-col items-center justify-center my-4">
+                <Card>
+                    <CardContent>
+                        You have no training plans yet.
+                    </CardContent>
+                </Card>
+                <Button onClick={() => logout()}>Logout</Button>
+            </div>  
         </div>
     )
 }
