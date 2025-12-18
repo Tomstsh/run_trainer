ecc982bf5ca71d2d8bda4a572515e6d318c68d33
Tomstsh <tshort2298@gmail.com>
2025-12-18 13:56:17 +0000

    make calendar events popovers with run details

--- src/frontend/src/components/PlanCalendar.tsx
+++ src/frontend/src/components/PlanCalendar.tsx
@@ -3,34 +3,34 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import { Card, CardContent } from "@/components/ui/card";
+import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
 
 function mapPlanToEvents(planJson: any) {
-    console.log(planJson);
     
     return planJson.weeks.flatMap((week) =>
         week.days.map((day: any) => {
             let color;
             switch (day.workout_type) {
                 case "easy":
-                    color = "lime";
+                    color = "#2D7DD2";
                     break
                 case "long":
-                    color = "purple";
+                    color = "#2E294E";
                     break
                 case "tempo":
-                    color = "orange";
+                    color = "#FF7F11";
                     break
                 case "interval":
-                    color = "black";
+                    color = "#2e4057";
                     break
                 case "rest":
-                    color = "green";
+                    color = "#66A182";
                     break
                 case "race":
-                    color = "red";
+                    color = "#FF101F";
                     break
                 default:
-                    color = "blue";
+                    color = "black";
                     break
             }
             return { title: day.workout_type,
@@ -56,11 +56,27 @@ export function PlanCalendar({planJson}: {planJson: any}) {
                     center: "title",
                     right: "dayGridMonth,dayGridWeek,dayGridDay"
                 }}
-                eventColor="blue"
                 height="auto"
                 dayCellClassNames="rounded-md border border-gray-500 hover:bg-gray-50"
-                eventClick={(info) => {
-                    alert(`Workout Details:\nType: ${info.event.extendedProps.description}`);
+                eventContent={(eventInfo) => {
+                    return (
+                        <Popover>
+                            <PopoverTrigger asChild>
+                                <div
+                                    className={`rounded-md px-2 py-0 border cursor-pointer text-sm`}
+                                >
+                                    {eventInfo.event.title}
+                                </div>
+                            </PopoverTrigger>
+                            <PopoverContent>
+                                <ul className="max-w-md space-y-2 text-body list-none list-inside">
+                                    <li>Description: {eventInfo.event.extendedProps.description}</li>
+                                    <li>Distance: {eventInfo.event.extendedProps.distance_km ? eventInfo.event.extendedProps.distance_km + " km" : "n/a"}</li>
+                                    <li>Intensity: {eventInfo.event.extendedProps.intensity} </li>
+                                </ul>
+                            </PopoverContent>
+                        </Popover>
+                    )
                 }}
             />
             <Card className="mt-4">
